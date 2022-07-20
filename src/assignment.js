e59b4f9fcc0024da5688cb87955c5a8159a13739
Amara Egbuna <95223342+JoannaAmy@users.noreply.github.com>
2022-07-20 22:44:46 +0000

    q2

--- src/assignment.js
+++ src/assignment.js
@@ -16,12 +16,7 @@ const assignment = {};
 let array = [1,2,3,4,5,6,7,8,9]
 
 function sumOfNumbers(arrayOfNumbers) {
-   
-  let sum = 0
-  for (let i = 0; i < arrayOfNumbers.length; i++)  
-    sum += arrayOfNumbers[i];  
-     
-  return sum;
+    return arrayOfNumbers.reduce((accumulator, number) => accumulator += number, 0)
 }
 sumOfNumbers(array)
 
